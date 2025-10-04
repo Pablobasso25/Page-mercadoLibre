42da7e4d10cd79706a83446cd66b327fda27f0ca
Pablo Basso <pablobasso25@gmail.com>
2025-10-04 00:14:54 -0300

    feat: set up basic admin panel layout

--- js/main.js
+++ js/main.js
@@ -1,77 +1,11 @@
-/* import { productos } from "./modules/productos.js";
-import { manejarRegistro } from "./modules/registro.js";
-import { manejarLogin } from './modules/login.js';
-import { restaurarSesion, actualizarNavbar, cerrarSesion } from "./modules/sesion.js";
 
-manejarLogin();
-restaurarSesion();
-actualizarNavbar();
-
-document.addEventListener("DOMContentLoaded", manejarRegistro);
-document.getElementById('btnCerrarSesion').addEventListener('click', cerrarSesion);
-
-
-const botonBuscar = document.getElementById("botonBuscar");
-const inputBuscador = document.getElementById("buscador");
-
-botonBuscar.addEventListener("click", () => {
-  const termino = inputBuscador.value.trim().toLowerCase();
-
-  const productoEncontrado = productos.find(p =>
-    p.nombre.toLowerCase().includes(termino)
-  );
-
-  if (productoEncontrado) {
-    window.location.href = `./pages/detalle.html?id=${productoEncontrado.id}`;
-  } else {
-    alert("No se encontró ningún producto con ese nombre.");
-  }
-}); */
-
-/* import { productos } from "./modules/productos.js";
-import { manejarRegistro } from "./modules/registro.js";
-import { manejarLogin } from './modules/login.js';
-import { restaurarSesion, actualizarNavbar, cerrarSesion } from "./modules/sesion.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  manejarLogin();
-  restaurarSesion();
-  actualizarNavbar();
-  manejarRegistro();
-
-  const botonCerrarSesion = document.getElementById('btnCerrarSesion');
-  if (botonCerrarSesion) {
-    botonCerrarSesion.addEventListener('click', cerrarSesion);
-  }
-
-  const botonBuscar = document.getElementById("botonBuscar");
-  const inputBuscador = document.getElementById("buscador");
-
-  if (botonBuscar && inputBuscador) {
-    botonBuscar.addEventListener("click", () => {
-      const termino = inputBuscador.value.trim().toLowerCase();
-
-      const productoEncontrado = productos.find(p =>
-        p.nombre.toLowerCase().includes(termino)
-      );
-
-      if (productoEncontrado) {
-        window.location.href = `./pages/detalle.html?id=${productoEncontrado.id}`;
-      } else {
-        alert("No se encontró ningún producto con ese nombre.");
-      }
-    });
-  }
-});
-
- */
-// 🔹 Importaciones necesarias
+// Importaciones necesarias
 import { productos } from "./modules/productos.js";
 import { manejarRegistro } from "./modules/registro.js";
 import { manejarLogin } from './modules/login.js';
 import { restaurarSesion, actualizarNavbar, cerrarSesion } from "./modules/sesion.js";
 
-// 🔹 Lógica principal que espera al DOM
+// Lógica principal que espera al DOM
 document.addEventListener("DOMContentLoaded", () => {
   manejarLogin();
   restaurarSesion(); // esta ya actualiza el navbar si restaura
